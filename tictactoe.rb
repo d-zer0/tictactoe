9ad4021ac0208c5f724837b3244c9786d8aa6be8
d-zer0 <daniel.varcas@gmail.com>
2016-05-11 17:16:22 +0100

    Added #mark_grid ('O' only), #play_game and #game_over

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,54 +1,65 @@
-def play
-	until game_over
+module Tictactoe
+
+	class Game
+		
+		@game_over = false
+
+			@@marks = {
+				"a1" => " ", 	"a2" => " ", 	"a3" => " ",	# top row
+				"b1" => " ", 	"b2" => " ", 	"b3" => " ",	# middle row
+				"c1" => " ", 	"c2" => " ", 	"c3" => " "		# bottom row
+			}
+
+		def play_game
+			until @game_over == true
+				show_grid
+				if @@marks.values.none? {|cell| cell == " "}
+					puts "Game Over!"
+					@game_over = true
+				else
+					mark_grid
+				end
+			end
+		end
+
+		def show_grid
+
+			vertical =		"       |     |    "
+			# 					 X  |  O  |  X
+			horizontal =	"  _____|_____|_____"
 
 
-def game_over
-	until Grid.marks.all? = " "
-		game_over = false
-	end
-end
+			puts "    1     2     3  "
+			puts vertical
+			puts "A   #{@@marks["a1"]}  |  #{@@marks["a2"]}  |  #{@@marks["a3"]}  "
+			puts horizontal
+			puts vertical
+			puts "B   #{@@marks["b1"]}  |  #{@@marks["b2"]}  |  #{@@marks["b3"]}  "
+			puts horizontal
+			puts vertical
+			puts "C   #{@@marks["c1"]}  |  #{@@marks["c2"]}  |  #{@@marks["c3"]}  "
+			puts vertical
+		end
 
-class Grid
-
-	@@vertical		= "       |     |    "
-	#          	           X  |  O  |  X
-	@@horizontal	= "  _____|_____|_____"
-
-	@@marks = {
-		"a1" => " ", 	"a2" => " ", 	"a3" => " ",	# rop row
-		"b1" => " ", 	"b2" => " ", 	"b3" => " ",	# middle row
-		"c1" => " ", 	"c2" => " ", 	"c3" => " "		# bottom row
-	}
-
-	def show
-		puts "    1     2     3  "
-		puts @@vertical
-		puts "A   #{@@marks["a1"]}  |  #{@@marks["a2"]}  |  #{@@marks["a3"]}  "
-		puts @@horizontal
-		puts @@vertical
-		puts "B   #{@@marks["b1"]}  |  #{@@marks["b2"]}  |  #{@@marks["b3"]}  "
-		puts @@horizontal
-		puts @@vertical
-		puts "C   #{@@marks["c1"]}  |  #{@@marks["c2"]}  |  #{@@marks["c3"]}  "
-		puts @@vertical
-	end
-
-	def mark
-		player_one = true
-
-		puts "Which cell?"
-		choice = gets.chomp.downcase
-
-		if player_one
-			@@marks[choice] = "O"
-		elsif player_two
-			@@marks[choice] = "X"
+		def mark_grid
+			puts "Which cell?"
+			choice = gets.chomp.downcase
+			if choice == "all"
+				@@marks["a1"] = "O"
+				@@marks["a2"] = "O"
+				@@marks["a3"] = "O"
+				@@marks["b1"] = "O"
+				@@marks["b2"] = "O"
+				@@marks["b3"] = "O"
+				@@marks["c1"] = "O"
+				@@marks["c2"] = "O"
+				@@marks["c3"] = "O"
+			else
+				@@marks[choice] = "O"
+			end
 		end
 	end
-
 end
 
-my_grid = Grid.new
-my_grid.show
-my_grid.mark
-my_grid.show
+game = Tictactoe::Game.new
+game.play_game
